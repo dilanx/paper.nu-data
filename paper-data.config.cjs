154fe357d632cdec2ee26559dec08c76d9fded2b
Dilan <dilan@dilanxd.com>
2023-05-05 18:43:57 -0500

    update script

--- paper-data.config.cjs
+++ paper-data.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'paper-data',
       script: './paper-data.js',
-      args: 'schedule -p',
+      args: 'schedule -p -m ./local/latest.min.json',
       combine_logs: true,
       out_file: '/logs/apps/paper-data.log',
       error_file: '/logs/apps/paper-data.err.log',
