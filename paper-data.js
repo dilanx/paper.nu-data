04d7269e8c05fb88d88078cba9d1c85ff2cf38e5
Dilan <dilan@dilanxd.com>
2023-10-27 14:45:39 -0500

    update for 2.0.1

--- paper-data.js
+++ paper-data.js
@@ -11,7 +11,7 @@ import { updatePlan } from './lib/plan.js';
 
 const argv = yargs(process.argv.slice(2))
   //.version(false)
-  .version('2.0.0')
+  .version('2.0.1')
   .usage('Usage: $0 [options]')
   .option('dir', {
     alias: 'd',
