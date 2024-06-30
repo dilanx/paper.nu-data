50dce9c7ee5421a0a96da114c6694a36fe32c11a
Dilan <dilan@dilanxd.com>
2024-06-29 21:52:32 -0400

    v2.0.2

--- paper-data.js
+++ paper-data.js
@@ -93,7 +93,7 @@ async function main() {
         planData,
         scheduleData: data,
         term,
-        isLatestTerm: true,
+        isLatestTerm: !!argv.term,
         subjectsUpdated,
       });
     }
